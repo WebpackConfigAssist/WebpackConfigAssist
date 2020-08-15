c1e8eaad3797f8715d34b702309c4821156dbc88
Patrick Mullen <prmullen20@gmail.com>
2020-08-14 18:23:11 -0700

    webpack works, added htmlwebpack plugin

--- server/server.js
+++ server/server.js
@@ -12,4 +12,14 @@ app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, '../client/index.html'));
 });
 
+
+// if (process.env.NODE_ENV === 'production') {
+//   // statically serve everything in the build folder on the route '/build'
+//   app.use('/build', express.static(path.join(__dirname, '../build')));
+//   // serve index.html on the route '/'
+//   app.get('/', (req, res) => {
+//     res.sendFile(path.join(__dirname, '../index.html'));
+//   });
+// }
+
 app.listen(3000, () => console.log('Listening on port 3000...'));
